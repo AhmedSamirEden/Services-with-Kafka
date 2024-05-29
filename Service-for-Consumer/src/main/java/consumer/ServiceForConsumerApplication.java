4e243660b4abbedd7a606011007f164fa0152055
AhmedSamir <ahmed.samir@eden-tech.io>
2024-05-29 18:03:00 +0300

    add

---
+++ Service-for-Consumer/src/main/java/consumer/ServiceForConsumerApplication.java
@@ -0,0 +1,13 @@
+package consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceForConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceForConsumerApplication.class, args);
+	}
+
+}
---
+++ Service-for-Producer/src/main/java/producer/ServicesForProducerApplication.java
@@ -0,0 +1,13 @@
+package producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesForProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesForProducerApplication.class, args);
+	}
+
+}
